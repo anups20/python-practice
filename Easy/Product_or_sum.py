7a0fbdbbe5c04f4d5afc0360ec8809c1a4426d67
Anupam Sharma <anupam410@outlook.com>
2025-11-29 22:36:14 +0530

    New program_291125

---
+++ Easy/Product_or_sum.py
@@ -0,0 +1,8 @@
+number1 = input("Emter first number: ")
+number2 = input("Enter second number: ")
+product = int(number1) * int(number2)
+if product <= 1000:
+    print("The result is:", product)
+else:
+    sum = int(number1) + int(number2)
+    print("The result is:", sum)
